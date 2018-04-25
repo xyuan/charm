22db9027d7d8a2063235ff92bd44b404809e50c7
Ronak Buch <rabuch2@illinois.edu>
2018-04-25 16:24:55 -0500

    Update random generation to use C++ <random>
    Change-Id: I9e6a3ccd16951763b7aa12a9338141096661a393

---
+++ src/conv-core/random.C
@@ -0,0 +1,46 @@
+#include <random>
+
+using namespace std;
+
+typedef uniform_real_distribution<double> Distribution;
+typedef minstd_rand Generator;
+
+extern "C" {
+#include "converse.h"
+
+  CpvStaticDeclare(Generator*, _defaultStream);
+  CpvStaticDeclare(Distribution*, distribution);
+
+  void CrnInit(void)
+  {
+    CpvInitialize(Generator*, _defaultStream);
+    CpvInitialize(Distribution*, distribution);
+    CpvAccess(distribution) = new Distribution(0.0, 1.0);
+    CpvAccess(_defaultStream) = new Generator(0); // This should probably be seeded with random_device
+  }
+
+  void CrnSrand(unsigned int seed)
+  {
+    CpvAccess(_defaultStream)->seed(seed);
+  }
+
+  int CrnRand(void)
+  {
+    return (int)(CrnDrand()*0x80000000U);
+  }
+
+  int CrnRandRange(int min, int max)
+  {
+    return uniform_int_distribution<int>(min, max)(*(CpvAccess(_defaultStream)));
+  }
+
+  double CrnDrand(void)
+  {
+    return (*CpvAccess(distribution))(*(CpvAccess(_defaultStream)));
+  }
+
+  double CrnDrandRange(double min, double max)
+  {
+    return uniform_real_distribution<double>(min, max)(*(CpvAccess(_defaultStream)));
+  }
+}
